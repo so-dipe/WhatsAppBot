8b5b5779ed79279cbfd3e49f720588027c80f7bc
sodipe <98396909+so-dipe@users.noreply.github.com>
2024-01-16 00:38:54 +0000

    Add webhook callback endpoint to process and reply to incoming messages

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
-from fastapi import FastAPI, HTTPException, Request
-import requests
+from fastapi import FastAPI, Request
 from config.config import Config
-
-app = FastAPI()
+from whatsapp.whatsapp_client import WhatsAppClient
 
 VERIFY_TOKEN = "your_whatsapp_verify_token"
+whatsapp_client = WhatsAppClient()
 
-@app.post("/")
-def index():
-    return "welcome to app"
+
+app = FastAPI()
 
 @app.get("/webhook/")
 def subscribe(request: Request):
@@ -16,3 +14,17 @@ def subscribe(request: Request):
     if request.query_params.get('hub.verify_token') == VERIFY_TOKEN:
         return int(request.query_params.get('hub.challenge'))
     return "Authentication failed. Invalid Token."
+
+@app.post("/webhook/")
+async def callback(request: Request):
+    print("callback is being called")
+    body = await request.json()
+    print(body)
+    response = whatsapp_client.process_notification(body)
+    if len(response) > 0:
+        for message in response:
+            reply = f"Hi, You sent: {message['message']['text']}"
+            whatsapp_client.send_message(message['from']['id'], reply)
+            print(f"Reply sent to {message['from']['id']}")
+
+    return {"status": "success"}, 200
