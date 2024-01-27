6e5bda0c12e50c2dbe91dc0e36f21530477fd60d
sodipe <98396909+so-dipe@users.noreply.github.com>
2024-01-27 02:47:14 +0000

    Add CURRENT_PATH and SERVICE_ACCOUNT_PATH variables to setup.py

--- app/language_models/google/setup.py
+++ app/language_models/google/setup.py
@@ -2,9 +2,13 @@ import vertexai
 import json
 from google.oauth2 import service_account
 from config.config import Config
+import os
+
+CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
+SERVICE_ACCOUNT_PATH = os.path.abspath(os.path.join(CURRENT_PATH, "../../..", Config.SERVICE_ACCOUNT_PATH))
 
 def initialize_vertexai():
-    with open(Config.SERVICE_ACCOUNT_PATH, "r") as f:
+    with open(SERVICE_ACCOUNT_PATH, "r") as f:
         credentials = json.load(f)
         credentials = service_account.Credentials.from_service_account_info(credentials)
 
