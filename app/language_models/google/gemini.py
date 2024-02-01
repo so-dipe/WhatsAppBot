d8810bd8321b4644e2ad1aa4b23852142383bc13
sodipe <98396909+so-dipe@users.noreply.github.com>
2024-02-01 11:59:07 +0000

    precommit

--- app/language_models/google/gemini.py
+++ app/language_models/google/gemini.py
@@ -1,4 +1,12 @@
-from vertexai.preview.generative_models import GenerativeModel, Image, ChatSession
+from vertexai.preview.generative_models import (
+    GenerativeModel,
+    Image,
+    ChatSession,
+    GenerationConfig,
+    HarmCategory,
+    HarmBlockThreshold,
+    ResponseBlockedError,
+)
 from ...redis.redis_client import RedisClient
 from .setup import initialize_vertexai, initialize_speech_client
 from ..chat_model import ChatModel
@@ -7,7 +15,8 @@ from google.cloud import speech
 
 speech_client = initialize_speech_client()
 
-def transcribe(audio : bytes):
+
+def transcribe(audio: bytes):
     print("audio type", type(audio))
     audio = speech.RecognitionAudio(content=audio)
     config = speech.RecognitionConfig(
@@ -19,8 +28,13 @@ def transcribe(audio : bytes):
     for result in response.results:
         return format(result.alternatives[0].transcript)
 
+
 class GeminiChatModel(ChatModel):
-    def __init__(self, model_name="gemini-pro-vision", system_prompt_file_path="assets/gemini-prompt.txt"):
+    def __init__(
+        self,
+        model_name="gemini-pro-vision",
+        system_prompt_file_path="assets/gemini-prompt.txt",
+    ):
         initialize_vertexai()
         self.model_name = model_name
         self.redis_client = RedisClient()
@@ -30,61 +44,75 @@ class GeminiChatModel(ChatModel):
             system_prompt_file_path = os.path.join(base_dir, system_prompt_file_path)
             with open(system_prompt_file_path, "r") as f:
                 self.system_prompt = f.read()
+        self.config = GenerationConfig(
+            temperature=0.9,
+            max_output_tokens=200,
+        )
+        self.safety_settings = {
+            HarmCategory(1): HarmBlockThreshold(2),  # hate speech - block all
+            # dangerous content - block none
+            HarmCategory(2): HarmBlockThreshold(4),
+            HarmCategory(3): HarmBlockThreshold(3),  # harassment - block most
+            # sexually explicit - block some
+            HarmCategory(4): HarmBlockThreshold(2),
+        }
 
     def init_chat(self):
         chat = self.model.start_chat()
         return chat
 
-    def get_chat_response(self, chat: ChatSession, prompt: str, image=None, audio=None) -> str:
-        if image:
-            image = Image.from_bytes(image)
-            try:
-                response = chat.send_message([prompt, image]).text
-            except Exception as e:
-                print(f"Error sending image: {str(e)}")
-                response = "Couldn't generate a response."
-        elif audio:
-            try:
-                text = transcribe(audio)
-                response = chat.send_message(text).text
-            except Exception as e:
-                print(f"Error sending audio: {str(e)}")
-                response = "Couldn't generate a response."
-        else:
-            prompt = f"<SYS> Context: {self.system_prompt} <SYS> \n Query: {prompt}"
-            try:
-                response = chat.send_message(prompt).text
-            except Exception as e:
-                print(f"Error sending message: {str(e)}")
-                response = "Couldn't generate a response."
-        return response
+    async def handle_image(self, chat: ChatSession, image: bytes) -> str:
+        image = Image.from_bytes(image)
+        try:
+            response = await chat.send_message_async(image)
+            response_text = response.text
+        except ResponseBlockedError as e:
+            print(f"Error generating response: {str(e)}")
+            response_text = (
+                "sorry, I've been modified to be stay away from certain topics."
+            )
+        except Exception as e:
+            print(f"Error sending image: {str(e)}")
+            response_text = (
+                "It seems my engineers are a dud. I can't see. Check back later."
+            )
+        return response_text
 
-    async def get_async_chat_response(self, chat: ChatSession, prompt: str, image=None, audio=None) -> str:
+    async def handle_audio(self, chat: ChatSession, audio: bytes) -> str:
+        pass
+
+    async def handle_text(self, chat: ChatSession, text: str) -> str:
+        try:
+            response = await chat.send_message_async(
+                text,
+                generation_config=self.config,
+                safety_settings=self.safety_settings,
+            )
+            response_text = response.text
+        except ResponseBlockedError as e:
+            print(f"Error generating response: {str(e)}")
+            response_text = (
+                "sorry, I've been modified to be stay away from certain topics."
+            )
+        except Exception as e:
+            print(f"Error sending message: {str(e)}")
+            response_text = (
+                "It seems my engineers are a dud. I can't speak. Check back later."
+            )
+        return response_text
+
+    async def get_async_chat_response(
+        self, chat: ChatSession, prompt: str, image=None, audio=None
+    ) -> str:
         if image:
-            image = Image.from_bytes(image)
-            try:
-                response = await chat.send_message_async([prompt, image])
-                response = response.text
-            except Exception as e:
-                print(f"Error sending image: {str(e)}")
-                response = "Couldn't generate a response."
+            return await self.handle_image(chat, image)
         elif audio:
-            try:
-                text = transcribe(audio)
-                response = await chat.send_message_async(text)
-                response = f"Transcript: {text} \n Response: {response.text}"
-            except Exception as e:
-                print(f"Error sending audio: {str(e)}")
-                response = "Couldn't generate a response."
+            return await self.handle_audio(chat, audio)
         else:
-            prompt = f"{self.system_prompt} \n {prompt}"
-            try:
-                response = await chat.send_message_async(prompt)
-                response = response.text
-            except Exception as e:
-                print(f"Error sending message: {str(e)}")
-                response = "Couldn't generate a response."
-        return response
+            return await self.handle_text(chat, prompt)
+
+    def get_chat_response():
+        pass
 
     def save_history(self, chat_id, chat_session):
         history = chat_session.history
@@ -94,8 +122,8 @@ class GeminiChatModel(ChatModel):
     def get_history(self, chat_id):
         chat_session = self.init_chat()
         data = self.redis_client.get_data(chat_id)
-        history = data['history']
-        if (history is False) or (data['model_name'] != self.model_name):
+        history = data["history"]
+        if (history is False) or (data["model_name"] != self.model_name):
             self.save_history(chat_id, chat_session)
         else:
             chat_session._history = history
